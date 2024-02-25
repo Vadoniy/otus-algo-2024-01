400ccc7c9f43de82119626fc583fe5b20f9c9838
Vadim Kurbatov <vadim.kurbatov@ipfdigital.com>
2024-02-25 17:11:23 +0300

    hw 4

---
+++ hw_3/Program.java
@@ -0,0 +1,47 @@
+package hw_3;
+
+import hw_3.model.FactorArray;
+import hw_3.model.IArray;
+import hw_3.model.MatrixArray;
+import hw_3.model.SingleArray;
+import hw_3.model.VectorArray;
+
+import java.util.Date;
+import java.util.Random;
+import java.util.stream.Stream;
+
+public class Program {
+
+    public static void main(String[] args) {
+        Stream.of(new SingleArray<Date>(), new VectorArray<Date>(), new FactorArray<Date>(), new MatrixArray<Date>())
+                        .forEach(Program::testIfArrayWorks);
+        Stream.of(new SingleArray<Date>(), new VectorArray<Date>(), new FactorArray<Date>(), new MatrixArray<Date>())
+                .forEach(Program::testAddArray);
+    }
+
+    private static void testIfArrayWorks(IArray<Date> array) {
+        var index = new Random().nextInt(1, 50);
+        var elementToAdd = new Date();
+
+        array.add(elementToAdd, index);
+        var addedElement = array.get(index);
+        if (addedElement != elementToAdd) {
+            throw new RuntimeException("Wrong added element");
+        }
+        var removedElement = array.remove(index);
+        if (removedElement != elementToAdd) {
+            throw new RuntimeException("Wrong removed element");
+        }
+    }
+
+    private static void testAddArray(IArray<Date> array) {
+        final var amountOfElementsToAdd = 100_000;
+        long start = System.currentTimeMillis();
+
+        for (int j = 0; j < amountOfElementsToAdd; j ++)
+            array.add(new Date());
+
+        System.out.println(array.getClass().getSimpleName() + " added " + amountOfElementsToAdd + " elements in: " +
+                (System.currentTimeMillis() - start) + " ms");
+    }
+}
