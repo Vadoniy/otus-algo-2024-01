6c75bc2b5ab0043dfdc4a2c631a71ac9ca2612bf
Vadoniy <shturman_1989@mail.ru>
2024-03-04 18:10:37 +0300

    hw 4 (#4)
    * hw 4
    
    * added wrapper for arrayList
    
    * refactoring

---
+++ hw_3/model/ArrayListWrapper.java
@@ -0,0 +1,37 @@
+package hw_3.model;
+
+import java.util.ArrayList;
+
+public class ArrayListWrapper<T> implements IArray<T> {
+
+    private ArrayList<T> arrayList;
+
+    public ArrayListWrapper() {
+        this.arrayList = new ArrayList<T>();
+    }
+
+    @Override
+    public int size() {
+        return arrayList.size();
+    }
+
+    @Override
+    public void add(T item) {
+        arrayList.add(item);
+    }
+
+    @Override
+    public T get(int index) {
+        return arrayList.get(index);
+    }
+
+    @Override
+    public void add(T item, int index) {
+        arrayList.add(index, item);
+    }
+
+    @Override
+    public T remove(int index) {
+        return arrayList.remove(index);
+    }
+}
---
+++ hw_3/model/FactorArray.java
@@ -0,0 +1,60 @@
+package hw_3.model;
+
+public class FactorArray<T> implements IArray<T> {
+
+    private Object[] array;
+    private int factor;
+    private int indexToAdd;
+
+    public FactorArray(int factor, int initLength) {
+        this.factor = factor;
+        array = new Object[initLength];
+        indexToAdd = 0;
+    }
+
+    public FactorArray() {
+        this(50, 10);
+    }
+
+    @Override
+    public int size() {
+        return array.length;
+    }
+
+    @Override
+    public void add(T item) {
+        if (indexToAdd == size())
+            resize();
+        array[indexToAdd] = item;
+        indexToAdd++;
+    }
+
+    @Override
+    public void add(T item, int index) {
+
+        while (index >= size()) {
+            resize();
+        }
+
+        array[index] = item;
+    }
+
+    @Override
+    public T remove(int index) {
+        var element = array[index];
+        array[index] = null;
+        return (T) element;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public T get(int index) {
+        return (T) array[index];
+    }
+
+    private void resize() {
+        Object[] newArray = new Object[array.length + (array.length * factor) / 100];
+        System.arraycopy(array, 0, newArray, 0, array.length);
+        array = newArray;
+    }
+}
