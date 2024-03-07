d842c57cae1e1ed494024f2aceda2026c118ea77
Vadoniy <shturman_1989@mail.ru>
2024-03-07 18:27:04 +0300

    Hw 4 (#6)
    * hw 4
    
    * added wrapper for arrayList
    
    * refactoring
    
    * hw_3 module is renamed to hw_4
    
    ---------
    
    Co-authored-by: Vadim Kurbatov <vadim.kurbatov@ipfdigital.com>

---
+++ hw_4/model/SingleArray.java
@@ -0,0 +1,56 @@
+package hw_4.model;
+
+public class SingleArray<T> implements IArray<T> {
+
+    private Object[] array;
+
+    public SingleArray() {
+        array = new Object[0];
+    }
+
+    @Override
+    public int size() {
+        return array.length;
+    }
+
+    @Override
+    public void add(T item) {
+        resize();
+        array[size() - 1] = item;
+    }
+
+    @Override
+    public void add(T item, int index) {
+
+        if (index >= size()) {
+            resize(index + 1);
+        }
+
+        array[index] = item;
+    }
+
+    @Override
+    public T remove(int index) {
+        var element = array[index];
+        array[index] = null;
+        return (T) element;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public T get(int index) {
+        return (T) array[index];
+    }
+
+    private void resize() {
+        Object[] newArray = new Object[size() + 1];
+        System.arraycopy(array, 0, newArray, 0, size());
+        array = newArray;
+    }
+
+    private void resize(int i) {
+        Object[] newArray = new Object[size() + i];
+        System.arraycopy(array, 0, newArray, 0, size());
+        array = newArray;
+    }
+}
---
+++ hw_4/model/VectorArray.java
@@ -0,0 +1,59 @@
+package hw_4.model;
+
+public class VectorArray<T> implements IArray<T> {
+
+    private Object[] array;
+    private int vector;
+    private int size;
+
+    public VectorArray(int vector) {
+        this.vector = vector;
+        array = new Object[0];
+        size = 0;
+    }
+
+    public VectorArray() {
+        this(10);
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void add(T item) {
+        if (size() >= array.length)
+            resize();
+        array[size-1] = item;
+        size++;
+    }
+
+    @Override
+    public void add(T item, int index) {
+        while (index >= size()) {
+            resize();
+        }
+        array[index] = item;
+    }
+
+    @Override
+    public T remove(int index) {
+        var element = array[index];
+        array[index] = null;
+        return (T)element;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public T get(int index) {
+        return (T)array[index];
+    }
+
+    private void resize() {
+        Object[] newArray = new Object[array.length + vector];
+        System.arraycopy(array, 0, newArray, 0, array.length);
+        array = newArray;
+        size = array.length;
+    }
+}
